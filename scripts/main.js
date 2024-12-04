9fca4c31afe262b011d8b1cc15df494f62e2fd18
Milo <167593567+Millymilo000@users.noreply.github.com>
2024-12-03 19:03:42 -0700

    Add new rule, new blog post, and prepare nav for 1.12

--- scripts/main.js
+++ scripts/main.js
@@ -51,4 +51,19 @@ var els = document.querySelectorAll('.external');
             els[i].setAttribute("target", "_blank");
         }
 
-// since this javascript file is just in everysingle page, i might change its name to something like "global.js" to add some extra things
+// thank you w3schools https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_dropdown_navbar_click
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+    document.getElementById("navDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropButton')) {
+    var navDropdown = document.getElementById("navDropdown");
+      if (navDropdown.classList.contains('show')) {
+        navDropdown.classList.remove('show');
+      }
+    }
+  }
